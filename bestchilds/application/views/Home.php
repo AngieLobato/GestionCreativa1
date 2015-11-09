3295cb5038589c6d9151e75b632890f1e77324a5
AngieLobato <angiielobato@gmail.com>
2015-11-08 19:06:33 -0600

    home css y js

--- bestchilds/application/views/Home.php
+++ bestchilds/application/views/Home.php
@@ -5,8 +5,17 @@
 	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 	    <meta name="viewport" content="width=device-width, initial-scale=1" />
 	    <title>Best Childs Magazine</title>
+	    <link href="<?=base_url();?>css/bootstrap.min.css" rel="stylesheet" /> 
 	</head>
 	<body>
-	<h1>Best Childs Magazine</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<h1 class='text-center'>Best Childs Magazine</h1>
+				</div>
+			</div>
+		</div>
+		<script src="<?=base_url();?>js/jquery.min.js" type="text/javascript"></script>
+    	<script src="<?=base_url();?>js/bootstrap.min.js" type="text/javascript"></script>
 	</body>
 </html>
